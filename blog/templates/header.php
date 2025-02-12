3cdc9e55fa246f8f6951fed7f26972eac6435639
Lucas Seco Alves <lucasseco46.2@gmail.com>
2025-02-12 20:39:00 -0300

    .

---
+++ blog/templates/header.php
@@ -0,0 +1,29 @@
+<?php
+include_once("helpers/url.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog Codar</title>
+    <link rel="stylesheet" href="<?= $BASE_URL ?>/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=League+Gothic&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <a href="<?= $BASE_URL ?>L" id = 'logo'>
+            <img src="<?= $BASE_URL ?>/img/logo.svg" alt="">
+        </a>
+        <nav>
+            <ul id ='navbar'>
+                <li><a href="<?= $BASE_URL ?>" class="nav-link">Home</a></li>
+                <li><a href="#" class="nav-link">Categorias</a></li>
+                <li><a href="#" class="nav-link">Sobre</a></li>
+                <li><a href="<?= $BASE_URL ?>/contato.php" class="nav-link">Contato</a></li>
+            </ul>
+        </nav>
+    </header>
